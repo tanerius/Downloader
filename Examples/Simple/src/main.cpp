0de4d5637fa0c3c5858fb526c0c74c0c8219c098
Taner Selim <tanerius@gmail.com>
2024-02-10 23:23:59 +0100

    Added example folder initially

---
+++ Examples/Simple/src/main.cpp
@@ -0,0 +1,60 @@
+#include "EZResume.h"
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <vector>
+#include <cstdio>
+#include <thread>
+#include <nlohmann/json.hpp>
+
+
+int result = -1;
+int dlID = -1;
+
+#ifdef WIN32 
+#define PathSeparator      "\\"
+#else
+#define PathSeparator      "/"
+#endif
+
+class CallBackHandlerTest : public EZResume::IDownloaderHandler
+{
+public:
+    int m_id = -1;
+    EZResume::DownloadResult m_lastResult = EZResume::DownloadResult::IDLE;
+    virtual ~CallBackHandlerTest() {}
+public:
+    void DownloadCompleted(const int id, const EZResume::DownloadResult result, const char*)
+    {
+        m_id = id;
+        m_lastResult = result;
+    }
+
+    void DownloadProgress(const int, const EZResume::ulong, const EZResume::ulong)
+    {
+
+    }
+};
+
+int main(int, char*[])
+{
+    EZResume::Downloader d;
+    EZResume::Configutation config;
+    CallBackHandlerTest* cbh = new CallBackHandlerTest();
+
+    std::string dataFile = std::string(".") + std::string(PathSeparator) + std::string("50MB.bin");
+    std::string metaFile = std::string(".") + std::string(PathSeparator) + std::string("50MB.tmp");
+    
+    std::remove(dataFile.c_str());
+    std::remove(metaFile.c_str());
+
+    d.Download(1, "https://home.tanerius.com/samples/files/50MB.bin", dataFile.c_str(), config,
+        cbh);
+
+    std::remove(dataFile.c_str());
+    std::remove(metaFile.c_str());
+
+    delete cbh;
+
+    return 0;
+}
